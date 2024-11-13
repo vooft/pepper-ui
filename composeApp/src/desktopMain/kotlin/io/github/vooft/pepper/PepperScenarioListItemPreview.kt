e99fc3cec7073307f707849ebe9260da53587b6d
vooft <52610+vooft@users.noreply.github.com>
2024-11-13 14:17:44 +0400

    Moved previews to desktop

--- composeApp/src/desktopMain/kotlin/io/github/vooft/pepper/PepperScenarioListItemPreview.kt
+++ composeApp/src/desktopMain/kotlin/io/github/vooft/pepper/PepperScenarioListItemPreview.kt
@@ -1,7 +1,7 @@
 package io.github.vooft.pepper
 
+import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import io.github.vooft.pepper.components.scenariolist.PepperTestScenarioListItem
 import io.github.vooft.pepper.preview.PreviewData.aPepperTestScenario
 
